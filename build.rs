8edb2fa519a937cb4ceecffbd2305391614ca6fb
Christopher Brown <ccbrown112@gmail.com>
2021-02-12 17:39:47 -0500

    move proto files

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
 fn main() {
     let proto_root = "protos";
     println!("cargo:rerun-if-changed={}", proto_root);
-    protoc_grpcio::compile_grpc_protos(&["protos/client.proto"], &[proto_root], "src/protos", None)
-        .expect("failed to compile grpc definitions");
+    protoc_grpcio::compile_grpc_protos(
+        &["protos/client/client.proto"],
+        &[proto_root],
+        "src/protos",
+        None,
+    )
+    .expect("failed to compile grpc definitions");
 }
